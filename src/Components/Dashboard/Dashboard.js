32a5348acd35a83681dc3ab0d1e0355f28254b64
YashMahale06 <yashmahale2002@gmail.com>
2023-01-29 15:58:50 +0530

    installed tailwind annd bootstrap

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -2,8 +2,9 @@ import React from "react";
 
 const Dashboard =()=>{
     return (
-        <div>
-        <h1>Welcome to Grampanchayat Website </h1>
+        <div className="mt-80">
+        <h1 className="text-blue-400 font-bold text-4xl">Welcome to Grampanchayat Website</h1>
+        
         </div>
     )
 }
